bb9e2b04da62279ec055293b7dfedfd25d0142db
ziolkowski-bartosz <ziolkowski.b4rtosz@gmail.com>
2023-07-26 14:19:25 +0200

    add api link to env variables

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "https://polenix-b105de561032.herokuapp.com/api",
+  uri: process.env.REACT_APP_APOLLO_URL,
 });
 
 const authLink = setContext((_, { headers }) => {
