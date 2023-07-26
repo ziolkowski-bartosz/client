82b158a5f2ea1749d776f920e66db9648abb5ba6
ziolkowski-bartosz <ziolkowski.b4rtosz@gmail.com>
2023-07-26 15:20:52 +0200

    moved api link to env variables

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -3,9 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri:
-    process.env.REACT_APP_APOLLO_URL ||
-    "https://polenix-b105de561032.herokuapp.com/api",
+  uri: process.env.REACT_APP_APOLLO_URL,
 });
 
 const authLink = setContext((_, { headers }) => {
